23804e2af07e24f569ec63799c51b7c3418dcf4b
Smartman Apps <20635275+SmartmanApps@users.noreply.github.com>
2023-07-23 08:45:30 +1000

    Add project files.

---
+++ CSharpUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharpUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ CSharpUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CSharpUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
